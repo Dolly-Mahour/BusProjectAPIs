7ef6f75244422e281296cf00528ad957195d3bf3
Dolly-Mahour <dollymahour282@gmail.com>
2026-05-19 17:53:17 +0530

    final change

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const verifyToken = require("../middleware/verifyToken");
+const verifyToken = require("../middleware/verifytoken");
 
 router.get("/verify-token", verifyToken, (req, res) => {
 
