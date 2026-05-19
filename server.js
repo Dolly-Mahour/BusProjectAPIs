72e791e5286370ae311546092f5fe4e2152e05ab
Dolly-Mahour <dollymahour282@gmail.com>
2026-05-19 17:39:55 +0530

    api port connection

--- server.js
+++ server.js
@@ -5,30 +5,33 @@ require("dotenv").config();
 
 const app = express();
 
-
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-const authRoutes = require("./routes/authRoutes");
-
-app.use("/", authRoutes);
 // Routes
+const authRoutes = require("./routes/authRoutes");
+app.use("/", authRoutes);
+
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/bustrips", require("./routes/tripRoutes"));
 app.use("/api/places", require("./routes/placeRoutes"));
 
-// MongoDB Connection
+// Root test route 
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
 mongoose.connect(process.env.MONGO_URL)
 .then(() => {
     console.log("MongoDB Connected");
+
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 })
 .catch((err) => {
-    console.log(err);
-});
-
-
-// Server Start
-app.listen(5000, () => {
-    console.log("Server Running On Port 5000");
+    console.log("MongoDB Connection Error:", err);
 });
